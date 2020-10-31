cc7b8120c518689688fca0b85e78c8dfbd6c8130
MikeVez <mikevez14@gmail.com>
2020-10-31 15:05:59 +0000

    get_resume removed for testing

--- app.py
+++ app.py
@@ -7,22 +7,25 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'recruitment'
-app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster.tzpih.mongodb.net/recruitment?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
+
+
+
+
+@app.route('/')
+def home():
+    recent_resumes=mongo.db.recipes.find().sort([("date",-1)]).limit(4)
+    
+    return render_template("home.html", recent_resumes=recent_resumes)
+
+@app.route('/add_resume')
+def add_resume():
+    return render_template('add_resume.html', resumes=mongo.db.job_sector_title.find().sort([("name", 1)]))
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
-            debug=False)
-
-@app.route('/')
-def home():
-   
-    return render_template("home.html")
-    
-
-@app.route('/add_recipe')
-def add_recipe():
-   
-    return render_template('add_resume.html', resumes=mongo.db.job_sector_title.find().sort([("name", 1)]))
+            debug=True)
