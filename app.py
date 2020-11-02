f89c7fe074f3503aa6038f16bc8999873b0dfe3e
MikeVez <mikevez14@gmail.com>
2020-11-02 10:01:50 +0000

    edit_resume details added to app.py

--- app.py
+++ app.py
@@ -7,50 +7,46 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'recruitment'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster.tzpih.mongodb.net/recruitment?retryWrites=true&w=majority'
-<<<<<<< HEAD
-
-mongo = PyMongo(app)
-=======
->>>>>>> e27eb2ab78049e6a1dd742f96074ff7a2599a203
-
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
-    recent_resumes=mongo.db.recipes.find().sort([("date",-1)]).limit(4)
-=======
    
 
     recent_resumes=mongo.db.resumes.find().sort([("date",-1)]).limit(4)
     return render_template("home.html", recent_resumes=recent_resumes)
 
->>>>>>> e27eb2ab78049e6a1dd742f96074ff7a2599a203
     
     return render_template("home.html", recent_resumes=recent_resumes)
 
 @app.route('/add_resume')
 def add_resume():
-    return render_template('add_resume.html', resumes=mongo.db.job_sector_title.find().sort([("name", 1)]))
+    return render_template('add_resume.html', resumes=mongo.db.resumes.job_sector_title.find().sort([("name", 1)]))
 
-<<<<<<< HEAD
+
+
+@app.route('/save_resume', methods=["POST"])
+def save_resume():   
+    resume=request.form.to_dict()
+    resume["date"]=datetime.datetime.now()
+    mongo.db.reumes.insert_one(resume)
+    return redirect(url_for("get_resume"))
+
+
+@app.route('/all_resumes/<resume_id>/delete', methods=["POST"])
+def delete_resume(resume_id):
+    mongo.db.resumes.remove({'_id': ObjectId(resume_id)})
+    return redirect(url_for('get_resume'))
+
+
+
+@app.route('/edit_resume/<resume_id>')
+def edit_resume(resume_id):
+    return render_template('edit_resume.html', resume=mongo.db.resumes.find_one({'_id': ObjectId(resume_id)}), job_title=mongo.db.resumes.job_sector_title.find().sort([("name", 1)]))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
-            debug=True)
-=======
-@app.route('/add_recipe')
-def add_recipe():
-   
-    return render_template('add_resume.html', resumes=mongo.db.job_sector_title.find().sort([("name", 1)]))
-
-
-
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=int(os.environ.get('PORT', '5001')),
             debug=False)
->>>>>>> e27eb2ab78049e6a1dd742f96074ff7a2599a203
